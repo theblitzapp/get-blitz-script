deccbc613bbc327c08d090f319ca23d162f5c891
MARCEL <16888873+1337MARCEL@users.noreply.github.com>
2019-03-06 23:51:20 +0100

    Rework
    - Introduce language map
    - Rework string map

--- index.js
+++ index.js
@@ -1,78 +1,104 @@
-/*
-  Define an easy structure for strings
-  We have limited strings so this structure should do well
-*/
-var firstStringMap = {
-  cs_CZ:
-    'Vyzkoušej naši BEZPLATNOU aplikaci: automatické runy, sady předmětů a statistiky.',
-  de_DE:
-    'Probiere unsere KOSTENLOSE Desktop App aus: Automatischer Import von Runen und Builds, so wie Unterstützung beim Gewinnen deiner Lane.',
-  es_ES:
-    'Prueba GRATIS nuestra aplicación de escritorio: runas automáticas, builds, y estadísticas de counter.',
-  fr_FR:
-    'Essayez notre application de bureau GRATUITE : runes automatiques, builds et des astuces pour gagner votre lane, tout ça totalement automatisé.',
-  ko_KR:
-    '저희의 무료 컴퓨터 앱을 사용해보세요:  자동 룬, 빌즈 그리고 상대 통계.',
-  pt_BR:
-    'Teste GRÁTIS nosso aplicativo para desktop: Runas, Arsenais e informações de counters de forma automática.',
-  pt_PT:
-    'Teste GRÁTIS nosso aplicativo para desktop: Runas, Arsenais e informações de counters de forma automática.',
-  ru_RU:
-    'Попробуй нашу БЕСПЛАТНУЮ Программу: автоматические руны, сборки, и контр-статистика.',
-  zh_CN:
-    '试试我们的免费桌面App：自动符文，出装，击杀等数据统计。',
-  zh_TW:
-    '試試我們的免費桌面App：自動符文，出裝，擊殺等資料統計。',
-  en_US:
-    'Try Our FREE Desktop App: automatic runes, builds, and counter stats.'
+/* Language map */
+var languageMap = {
+  'bg': 'bg-BG',
+  'bg-BG': 'bg-BG',
+  'cs': 'cs-CZ',
+  'cs-CZ': 'cs-CZ',
+  'de': 'de-DE',
+  'de-AT': 'de-DE',
+  'de-DE': 'de-DE',
+  'de-CH': 'de-DE',
+  'en-US': 'en-US',
+  'el': 'el-GR',
+  'el-GR': 'el-GR',
+  'es': 'es-ES',
+  'es-AR': 'es-ES',
+  'es-419': 'es-ES',
+  'es-CL': 'es-ES',
+  'es-CO': 'es-ES',
+  'es-EC': 'es-ES',
+  'es-ES': 'es-ES',
+  'es-LA': 'es-ES',
+  'es-MX': 'es-ES',
+  'es-NI': 'es-ES',
+  'es-US': 'es-ES',
+  'es-VE': 'es-ES',
+  'fi': 'fi-FI',
+  'fi-FI': 'fi-FI',
+  'fr': 'fr-FR',
+  'fr-BE': 'fr-FR',
+  'fr-CA': 'fr-FR',
+  'fr-CH': 'fr-FR',
+  'fr-FR': 'fr-FR',
+  'ko-KR': 'ko-KR',
+  'pl': 'pl-PL',
+  'pl-PL': 'pl-PL',
+  'pt': 'pt-PT',
+  'pt-BR': 'pt-BR',
+  'pt-PT': 'pt-PT',
+  'ro': 'ro-RO',
+  'ro-RO': 'ro-RO',
+  'ru': 'ru-RU',
+  'ru-RU': 'ru-RU',
+  'sk': 'sk-SK',
+  'sk-SK': 'sk-SK',
+  'zh': 'zh-CN',
+  'zh-CN': 'zh-CN',
+  'zh-Hans': 'zh-CN',
+  'zh-Hant': 'zh-TW',
+  'zh-HK': 'zh-TW',
+  'zh-SG': 'zh-CN',
+  'zh-TW': 'zh-TW'
 };
 
-var secondStringMap = {
-  cs_CZ: 'Dozvědět se víc',
-  de_DE: 'Erfahre mehr',
-  es_ES: 'Leer más',
-  fr_FR: 'En apprendre plus',
-  ko_KR: '더 알아보기',
-  pt_BR: 'Saiba mais',
-  pt_PT: 'Saiba mais',
-  ru_RU: 'Узнай больше',
-  zh_CN: '获取更多信息',
-  zh_TW: '獲取更多資訊',
-  en_US: 'Learn more'
+/* String map */
+var stringMap = {
+  'bg-BG': ['Пробвайте нашето десктоп приложение: автоматични руни, бюлдове, и каунтър статистики', 'Aflați mai multe'],
+  'cs-CZ': ['Vyzkoušej naši BEZPLATNOU aplikaci: automatické runy, sady předmětů a statistiky.', 'Dozvědět se víc'],
+  'de-DE': ['Probiere unsere KOSTENLOSE Desktop-App: automatische Runen, Builds und Counter-Statistiken.', 'Erfahre mehr'],
+  'es-ES': ['Prueba GRATIS nuestra aplicación de escritorio: runas automáticas, builds, y estadísticas de counter.', 'Leer más'],
+  'fi-FI': ['Testaa meidän ILMAISTA työpöytäsovellusta: automaattiset runet, buildit ja vastastrategiat.', 'Opi lisää'],
+  'fr-FR': ['Essayez notre application GRATUITE: runes automatiques, builds, et stats des counter.', 'En apprendre plus'],
+  'en-US': ['Try Our FREE Desktop App: automatic runes, builds, and counter stats.', 'Learn more'],
+  'el-GR': ['Δοκιμάστε τη δωρεάν εφαρμογή υπολογιστή:αυτόματοι ρούνοι,αντικείμενα και στατιστικά παιξίματος.', 'Μάθετε περισσότερα.'],
+  'ko-KR': ['저희의 무료 컴퓨터 앱을 사용해보세요:  자동 룬, 빌즈 그리고 상대 통계.', '더 알아보기'],
+  'pl-PL': ['Przetestuj nasza DARMOWA aplikacje na komputer: automatyczne runy, ekwipunek oraz statystyki przeciwnika', 'Dowiedz sie wiecej'],
+  'pt-BR': ['Teste GRÁTIS nosso aplicativo para desktop: Runas, Arsenais e informações de counters de forma automática.', 'Saiba mais'],
+  'pt-PT': ['Experimenta a nossa aplicação gratuita para Desktop: runas automáticas, builds e dicas para vencer.', 'Sabe mais'],
+  'ro-RO': ['Încercați aplicația noastră gratuită Desktop: rune automate, build-uri și statistici contra adversarilor.', 'Aflați mai multe'],
+  'ru-RU': ['Попробуйте нашу БЕСПЛАТНУЮ программу: автоматические руны, билды, контрпики.', 'Узнать больше'],
+  'sk-SK': ['Vyskúšaj našu BEZPLATNÚ aplikáciu: automatické runy, sady predmetov a štatistiky.', 'Dozvedieť sa viac'],
+  'zh-CN': ['试试我们的免费桌面App：自动符文，出装，击杀等数据统计。', '获取更多信息'],
+  'zh-TW': ['試試我們的免費桌面App：自動符文，出裝，擊殺等資料統計。', '獲取更多資訊']
 };
 
-/*
-  Get language from navigator
-*/
+/* Get language from navigator */
 function getLanguage() {
-  var language = 'en_US';
+  var languageCode;
   if (navigator.languages && navigator.languages.length) {
-    language = navigator.languages[0];
+    languageCode = navigator.languages[0];
   } else if (navigator.userLanguage) {
-    language = navigator.userLanguage;
+    languageCode = navigator.userLanguage;
   } else if (navigator.language) {
-    language = navigator.language;
+    languageCode = navigator.language;
   }
+  var language = languageMap[languageCode] || 'en-US';
   return language;
 }
 
-/*Default language*/
-var DEFAULT_LANGUAGE = 'en_US';
-
-/*For the map objects defined above, this function will give the right string */
-function getStringFromMap(strMap) {
+/* For the map objects defined above, this function will give the right string */
+function getStringFromMap(strMap, pos) {
   var language = getLanguage();
-  return strMap[language] || strMap[DEFAULT_LANGUAGE];
+  return strMap[language][pos] || strMap[DEFAULT_LANGUAGE][pos];
 }
 
-/*Build the Blitz header */
+/* Build the Blitz header */
 function buildBlitzHeader() {
-  var image =
-    '<img src="https://s3.amazonaws.com/solomid-resources/probuilds/img/blitzheaderlogo.png">';
-  var firstString = getStringFromMap(firstStringMap);
-  var secondString = getStringFromMap(secondStringMap);
+  var image = '<img src="https://s3.amazonaws.com/solomid-resources/probuilds/img/blitzheaderlogo.png">';
+  var firstString = getStringFromMap(stringMap, 0);
+  var secondString = getStringFromMap(stringMap, 1);
   return '<p>' + image + firstString + '&nbsp; <u>' + secondString + '</u></p>';
 }
 
-/*Set the blitz header */
+/* Set the blitz header */
 document.getElementById('blitzBanner').innerHTML = buildBlitzHeader();
